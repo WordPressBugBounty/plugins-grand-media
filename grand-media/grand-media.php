f404559185fe9bfa4de07f3204fbee93563c38b9
Sean Murphy <sean@iamseanmurphy.com>
2025-10-01 20:31:33 +0000

    1.24.0

--- grand-media/grand-media.php
+++ grand-media/grand-media.php
@@ -3,7 +3,7 @@
  * Plugin Name: Gmedia Gallery
  * Plugin URI: http://wordpress.org/extend/plugins/grand-media/
  * Description: Gmedia Gallery - powerful media library plugin for creating beautiful galleries and managing files.
- * Version: 1.23.0
+ * Version: 1.24.0
  * Author: Rattus
  * Author URI: https://codeasily.com/
  * Requires at least: 5.3.0
@@ -39,7 +39,7 @@ if ( ! class_exists( 'Gmedia' ) ) {
 	 */
 	class Gmedia {
 
-		public $version       = '1.23.0';
+		public $version       = '1.24.0';
 		public $dbversion     = '1.8.0';
 		public $minium_WP     = '5.3';
 		public $options       = '';
